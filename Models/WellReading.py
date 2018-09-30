7283c9bbba71de7c2977dfdc8bbce205ada1fdfa
Zakaria Talhami <Zakaria.a.a.talhami@gmail.com>
2018-10-01 00:55:29 +0300

    Edited init to include optional reading_id and volume

--- Models/WellReading.py
+++ Models/WellReading.py
@@ -6,22 +6,41 @@ logger = logging.getLogger(__name__)
 
 
 class WellReading():
-    def __init__(self, well, raw_data):
+    def __init__(self, well, raw_data, reading_id=0, valume=0):
         logger.info("Init Well Reading")
-        logger.debug("Init WellReading({},{})".format(well,raw_data))
+        logger.debug("Init WellReading({},{})".format(well, raw_data))
         if isinstance(well, Well.Well):
             self.__well = well
         else:
             logger.error("Invalid well reference")
             logger.debug("{} is an invalid Well Reference".format(well))
             raise ValueError
-        self.__volume = self.calculate_volume(raw_data)
+        self.__level = raw_data
+        self.__id = reading_id
+        if valume == 0:
+            self.__volume = self.calculate_volume(raw_data)
+        else:
+            self.__volume = valume
+
+    def __str__(self):
+        logger.info("WellReading toString")
+        reading = self.__well.__str__()
+        reading += "\n\t Volume = {}".format(self.__volume)
+        logger.debug("\n" + reading)
+        return reading
+
+    def __repr__(self):
+        logger.info("WellReading toString")
+        reading = self.__well.__str__()
+        reading += "\n\t Volume = {}".format(self.__volume)
+        logger.debug("\n" + reading)
+        return reading
 
     def calculate_volume(self, raw_data):
         logger.info("Calculating Volume")
-        if isinstance(raw_data, float) or isinstance(raw_data,int):
+        if isinstance(raw_data, float) or isinstance(raw_data, int):
             if raw_data > 0:
-                return self.__well.get_area() * raw_data
+                return round(self.__well.get_area() * raw_data, 3)
         logger.error("Invalid raw data")
         logger.debug("{} is an invalid raw data");
         raise ValueError
@@ -29,12 +48,15 @@ class WellReading():
     def get_well(self):
         return self.__well
 
-    def __str__(self):
-        logger.info("WellReading toString")
-        reading = self.__well.__str__()
-        reading += "\n\t Volume = {}".format(self.__volume)
-        logger.debug("\n"+reading)
-        return reading
-
     def get_volume(self):
         return self.__volume
+
+    def set_volume(self, val):
+        if isinstance(val, float):
+            self.__volume = val
+
+    def get_level(self):
+        return self.__level
+
+    def get_id(self):
+        return self.__id
