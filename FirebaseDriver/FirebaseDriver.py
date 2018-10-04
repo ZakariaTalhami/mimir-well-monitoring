bdef63aeb563d377cc7d838a84df294c5a8fb861
Zakaria Talhami <Zakaria.a.a.talhami@gmail.com>
2018-10-04 08:29:02 +0300

    Added Firebase helper classes

---
+++ FirebaseDriver/FirebaseDriver.py
@@ -0,0 +1,26 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CloudConnect:
+    def __init__(self):
+        logger.info("Init Cloud Connect")
+
+    def save_well(self, well):
+        logger.info("Persisting well to cloud")
+
+        return True
+
+    def read_well(self, well_id):
+        logger.info("check database for well with id = {}".format(well_id))
+
+        return True
+
+    def read_all_wells(self):
+        logger.info("Reading all the well data from firebase")
+        well_list = []
+        return well_list
+
+    def save_reading(self, reading):
+        return True
