a356c304a0ccc85b03425d965d5b80e96fdb2b73
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2018-11-19 10:25:53 +0000

    Added Initializer that is run on login

---
+++ Initializer.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This file is placed in the /home/<user> with the script
+# .profile, which should call this script.
+
+# Prevent SSH logins from triggering this script
+if [ -z "$SSH_CLIENT" ]
+then
+	# Kill any running python scripts
+	ps aux | grep "[p]ython" | awk '{command=sprintf("kill %d", $2);system(command)}'
+	echo "Running the code"
+	
+	# Initialize pigpiod
+	sudo killall pigpiod
+	sudo pigpiod
+	
+	# Make sure of working directory
+	cd $(dirname $0)
+	
+	# Run Gateway scripts
+	cd mimir-well-monitoring
+	python testing.py &
+	python i2c_module.py &
+	cd ..
+	
+	# Save running status
+	ps aux | grep "[p]ython"  > runningGatewayScripts.txt
+
+fi
