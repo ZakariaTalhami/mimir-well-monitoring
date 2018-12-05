de1397b962b68d9ed5550ca3527f8119f47ba9c9
Zakaria Talhami <Zakaria.a.a.talhami@gmail.com>
2018-12-05 11:53:53 +0200

    Added Cards and graphes in the Dashboard, implemented only for 4 wells

--- src/app/views/base/cards.component.ts
+++ src/app/views/base/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FireUpService } from '../../services/fire-up.service';
 import { Well } from '../../models/well';
 import { Observable } from 'rxjs'
@@ -7,34 +7,47 @@ import { Observable } from 'rxjs'
   selector: 'card-comp',
   templateUrl: 'cards.component.html'
 })
-export class CardsComponent {
-  @Input() Testing: string;
+export class CardsComponent  implements OnInit{
 
   wells: Well[];
-  wellID: string;
+  @Input() wellID: string;
   reading: number;
   volume: number;
 
   temp:Observable<Well>;
 
-  constructor(private wellDAO: FireUpService) {
+  constructor(private wellDAO: FireUpService)  {
+    // console.log(this.wellID);
+    
 
-    this.wellDAO.getWells().subscribe(item => {
-      console.log(item);
-      
-      this.wells = item;   
-      this.wellID = item.pop().id
 
-      this.wellDAO.getReadings(this.wellID).subscribe(data =>{
-        console.log(data);
-        let levels = data.map(item => item.Level);
-        let vol = data.map(item => item.Volume);
-        console.log(levels);
-        this.reading = levels.pop();
-        this.volume = vol.pop();
-      } );
+    // this.wellDAO.getWells().subscribe(item => {
+    //   console.log(item);
       
-    });
+    //   this.wells = item;   
+    //   this.wellID = item.pop().id
+
+    //   this.wellDAO.getReadings(this.wellID).subscribe(data =>{
+    //     console.log(data);
+    //     let levels = data.map(item => item.Level);
+    //     let vol = data.map(item => item.Volume);
+    //     console.log(levels);
+    //     this.reading = levels.pop();
+    //     this.volume = vol.pop();
+    //   } );
+      
+    // });
+  }
+
+  ngOnInit(){
+    this.wellDAO.getReadings(this.wellID).subscribe(data =>{
+          // console.log(data);
+          let levels = data.map(item => item.Level);
+          let vol = data.map(item => item.Volume);
+          // console.log(levels);
+          this.reading = levels.pop();
+          this.volume = vol.pop();
+        } );
   }
 
 }
